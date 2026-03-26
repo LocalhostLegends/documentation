ff1ac1d006e190fc18833084f14d091ed97dc8b3
KaratSergio <KaratSergio@users.noreply.github.com>
2026-03-26 12:53:52 +0000

    deploy: 69b22a95d1fde91a9c6b42df3491ddf45c1b95fd

--- .docusaurus/client-modules.js
+++
@@ -1,7 +0,0 @@
-export default [
-  require("E:\\Git_Hub\\documentation\\.docusaurus\\docusaurus-plugin-css-cascade-layers\\default\\layers.css"),
-  require("E:\\Git_Hub\\documentation\\node_modules\\infima\\dist\\css\\default\\default.css"),
-  require("E:\\Git_Hub\\documentation\\node_modules\\@docusaurus\\theme-classic\\lib\\prism-include-languages"),
-  require("E:\\Git_Hub\\documentation\\node_modules\\@docusaurus\\theme-classic\\lib\\nprogress"),
-  require("E:\\Git_Hub\\documentation\\src\\css\\custom.css"),
-];
